53c31d1c1fa8bd056cee6c74973993a0fa77e80f
radicalroy <roy@thexcompany.com>
2025-02-26 13:48:38 -0800

    updated dependencies (even tho i may regret later) and started button stories

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,3 +1,9 @@
-export const Button = (props: any) => {
+import type { ComponentProps } from 'react';
+
+export type ButtonProps = ComponentProps<'button'> & {
+  variant: 'primary' | 'secondary' | 'destructive';
+};
+
+export const Button = (props: ButtonProps) => {
   return <button {...props} />;
 };
